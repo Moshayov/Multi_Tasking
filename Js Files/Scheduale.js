04740a09cf5beefaf3e929e8626b3062f8fa8c7e
Moshayov <Moshayov@g.jct.ac.il>
2024-04-02 11:12:18 +0300

    fixing

--- Js Files/Scheduale.js
+++ Js Files/Scheduale.js
@@ -23,11 +23,7 @@ const app = {
         document.getElementById(currentPage).dispatchEvent(app.show);
     },
     pageShown: function(ev){
-      let currentPage = location.hash.replace('#' ,'');
-      console.log(currentPage);
-     if(currentPage=="Sign_Up"){
-      formContainer = document.querySelector(".form_container");
-     }
+    
   },
     poppin: function(ev){
       console.log(location.hash, 'popstate event');
@@ -44,15 +40,13 @@ const app = {
 document.addEventListener('DOMContentLoaded', app.init);
 
 home = document.querySelector("#home"),
-  
   signupBtn = document.querySelector("#signup"),
   pwShowHide = document.querySelectorAll(".pw_hide"),
   loginButton = document.querySelector("#login_now"),
-  signup_Button = document.querySelector("#signup_Now");
-  var loginForm = document.getElementById("#home");
-  
-  const EXPMINUTES=2;
-  let numOfTry=0;
+  signup_Button = document.querySelector("#signup_Now"),
+  signup = document.querySelector('#Sign_Up'),
+  loginForm = document.getElementById("#home");
+  signup.classList.add("show");
   home.classList.add("show");
 
 
@@ -72,12 +66,7 @@ function login(){
         if (user.username == usernameValue&& user.password==passwordValue) {
             alert("welcome");
             localStorage.setItem("username", JSON.stringify(username));
-            // Show the todo list
             console.log("hiiii");
-            //loginForm.style.display = "none";
-
-            //todo.style.display = "block";
-            //show_tasks(username);
         } else {
             alert("wrong");
         }
@@ -86,50 +75,41 @@ function login(){
   }
 
   function sign_up(){
-    
     let flage= false;
     let usernameValue =  document.querySelector("#username_sign_Up").value.trim();
     let passwordValue = document.querySelector("#password_signUp").value.trim();
     let passwordConfirmValue = document.querySelector("#Confirm_password").value.trim();
-    while(!flage)
-    {
-        let usernameValue =  document.querySelector("#username_sign_Up").value.trim();
-        let passwordValue = document.querySelector("#password_signUp").value.trim();
-        let passwordConfirmValue = document.querySelector("#Confirm_password").value.trim();
-        console.log(usernameValue);
-        console.log(passwordConfirmValue);
-        console.log(passwordValue);
-        if (!usernameValue || !passwordValue|| !passwordConfirmValue) {
-          alert("Please enter username  password and password Confirm.");
-          flage=false;
-          break;
-        }
-  
-        if (passwordConfirmValue != passwordValue) {
+    console.log(usernameValue);
+    console.log(passwordConfirmValue);
+    console.log(passwordValue);
+    if (!usernameValue || !passwordValue|| !passwordConfirmValue) {
+        alert("Please enter username  password and password Confirm.");
+         flage=false;
+    }
+    if (passwordConfirmValue != passwordValue) {
           console.log(passwordConfirmValue);
           console.log(passwordValue);
           alert("The passwords you entered do not match, please re-enter");
-          
           flage=false;
           return;
-        }
-        const fxhr = new FXMLHttpRequest()
-        fxhr.open("GET", "");
-        fxhr.send("usernames", (user) => {
-            console.log(user);
-            if (user.username == usernameValue&& user.password==passwordValue) {
-                alert("the user already exist in the system");
-                localStorage.setItem("username", JSON.stringify(username));
-                console.log("iiii");
-            } else {
+    }
+    const fxhr = new FXMLHttpRequest()
+    fxhr.open("GET", "");
+    fxhr.send("usernames", (user) => {
+    console.log(user);
+    if (user.username == usernameValue && user.password==passwordValue) {
+        alert("the user already exist in the system");
+            localStorage.setItem("username", JSON.stringify(username));
+            console.log("iiii");
+    } else 
+    {
                 flage=true;
-              }
-        });
-  }
+    }
+    });
   if(flage){
     let user = new User(usernameValue,passwordValue);
     fxhr.open("POST", "",true,user,null,null);
-    fxhr.send("add user", (u) => {
+    fxhr.send("add user", () => {
         console.log("the user added suucsefuly");
     });
   }
